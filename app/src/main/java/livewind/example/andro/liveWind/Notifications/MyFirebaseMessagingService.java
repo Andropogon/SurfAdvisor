a7dad6f50823a880260ea15df4e6791c096aaa68
Jan Gąsienica-Józkowy <jgjozkowy@gmail.com>
2019-04-08 20:08:30 +0200

    Notifications about new coverage - 1.7 -Notifications topics created. -Notifications show method created -Notifications intent working -Possibility to comment / like / join coverage from notification working. -Back navigation to catalogActivity from eventActivity from notification -Comments bug fixed -MyFirebaseMessagingService Class refactored, header and comments added. TODO: -Improve topics system -Test notification open on few devices in same time

--- app/src/main/java/livewind/example/andro/liveWind/Notifications/MyFirebaseMessagingService.java
+++ app/src/main/java/livewind/example/andro/liveWind/Notifications/MyFirebaseMessagingService.java
@@ -9,32 +9,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Build;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
-import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.MutableData;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.Transaction;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import livewind.example.andro.liveWind.CatalogActivity;
 import livewind.example.andro.liveWind.Event;
@@ -43,12 +27,22 @@ import livewind.example.andro.liveWind.R;
 import livewind.example.andro.liveWind.user.Windsurfer;
 
 import static livewind.example.andro.liveWind.ExtraInfoHelp.putCoverageToIntent;
-import static livewind.example.andro.liveWind.ExtraInfoHelp.putInfoToIntent;
 import static livewind.example.andro.liveWind.ExtraInfoHelp.putWindsurferToIntent;
 
+/**
+ * Created by JGJ on 10/11/18.
+ * Header added during refactoring add 08/04/2019 by JGJ.
+ *
+ * Receive and show notifications in correct way.
+ * Currently notifications types:
+ * -New like notification
+ * -New coverage notification
+ *
+ */
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    private String TAG = "MY_FIREBASE_MESSAGING_SERVICE_TAG";
-    final String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+    final String NOTIFICATION_NEW_LIKE_CHANNEL_ID = "like_noti_id";
+    final String NOTIFICATION_NEW_COVERAGE_CHANNEL_ID = "coverage_noti_id";
     /**
      * Notification codes
      */
@@ -56,10 +50,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     final static String NOTIFICATION_NEW_COVERAGE = "NEW_COVERAGE";
     private FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference mUsersDatabaseReference = mFirebaseDatabase.getReference().child("users");
-    private DatabaseReference mUsersNicknamesDatabaseReference = mFirebaseDatabase.getReference().child("usernames");
     private Windsurfer mWindsurfer= new Windsurfer();
 
-    public MyFirebaseMessagingService(){};
+    public MyFirebaseMessagingService(){}
+
     /**
      * Called if InstanceID token is updated. This may occur if the security of
      * the previous token had been compromised. Note that this is called when the InstanceID token
@@ -73,100 +67,79 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         sendRegistrationToServer(token);
     }
 
+    /**
+     * Send registration token to users database
+     * @param token
+     */
     private void sendRegistrationToServer(String token) {
         SharedPreferences displayOptions = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         SharedPreferences.Editor editor = displayOptions.edit();
         editor.putString(getString(livewind.example.andro.liveWind.R.string.user_tokenId_shared_preference),token);
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String userUid = sharedPref.getString(getApplicationContext().getString(livewind.example.andro.liveWind.R.string.user_uid_shared_preference),"DEFAULT");
-        Log.d(TAG, "Refreshed token of " + userUid + " TOKEN: " + token);
-
-        mUsersDatabaseReference.child(userUid).child("tokenId").setValue(token, new DatabaseReference.CompletionListener() {
-            @Override
-            public void onComplete(DatabaseError databaseError, DatabaseReference reference) {
-                if (databaseError != null) {
-                    Log.e(TAG, "Failed to write message", databaseError.toException());
-                }
-            }
-        });
+        mUsersDatabaseReference.child(userUid).child("tokenId").setValue(token);
     }
 
-
+    /**
+     * Method that check user notification settings and call correct showNotification method.
+     * @param remoteMessage - received message
+     */
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        boolean notificationsBoolean = sharedPref.getBoolean(getApplicationContext().getString(R.string.settings_notifications_allow_key), true);
         // Check if message contains a data payload.
-        Log.i("NOTIFICATION",Boolean.toString(notificationsBoolean));
         if (remoteMessage.getData().size() > 0) {
             Map<String, String> payload = remoteMessage.getData();
+            //Check that user allow notifications
+            //TODO split notifications switch about new coverage and new like
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            boolean notificationsBoolean = sharedPref.getBoolean(getApplicationContext().getString(R.string.settings_notifications_allow_key), true);
             if (notificationsBoolean) {
+                //Check what type of notification is this one
                 String payloadCode = payload.get("notificationCode");
                 switch (payloadCode) {
                     case NOTIFICATION_NEW_LIKE:
-                        showNotification(payload);
+                        showNewLikeNotification(payload);
                         break;
                     case NOTIFICATION_NEW_COVERAGE:
                         showNewCoverageNotification(payload);
                         break;
                     default:
-                        showNotification(payload);
+                        showNewLikeNotification(payload);
                 }
-            }else{
             }
-/**
- if ( Check if data needs to be processed by long running job  true) {
- // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
- scheduleJob();
- } else{
- // Handle message within 10 seconds
- handleNow();
- }
- */
-
         }
-/**
- // Check if message contains a notification payload.
- if (remoteMessage.getNotification() != null) {
- Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
- }
-
- // Also if you intend on generating your own notifications as a result of a received FCM
- // message, here is where that should be initiated. See sendNotification method below.
- */
-
     }
 
-    private void showNotification(Map<String, String> payload) {
+    /**
+     * Create and show notification about new like under your coverage
+     * @param payload Map which has the message payload in it
+     */
+    private void showNewLikeNotification(Map<String, String> payload) {
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
-
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_NEW_LIKE_CHANNEL_ID, "New like notification", NotificationManager.IMPORTANCE_DEFAULT);
             // Configure the notification channel.
-            notificationChannel.setDescription("Channel description");
+            notificationChannel.setDescription("Notification channel for new like notification");
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.RED);
-            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.setVibrationPattern(new long[]{0, 500, 500, 500});
             notificationChannel.enableVibration(true);
             notificationManager.createNotificationChannel(notificationChannel);
         }
 
-        // Create an explicit intent for an Activity in your app
         Intent intent = new Intent(this, CatalogActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_NEW_LIKE_CHANNEL_ID);
 
         notificationBuilder.setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_ALL)
                 .setWhen(System.currentTimeMillis())
                 .setSmallIcon(R.drawable.ic_thumb_up_black_24dp)
+                .setColor(getColor(R.color.light_green))
                 .setLargeIcon(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.app_icon_v3))
                 .setTicker(getApplicationContext().getString(R.string.notification_relation_liked_title))
-                //     .setPriority(Notification.PRIORITY_MAX)
                 .setContentTitle(getApplicationContext().getString(R.string.notification_relation_liked_title))
                 .setContentText(getApplicationContext().getString(R.string.notification_relation_liked_text1) +" "+ payload.get("eventName") +" "+ getApplicationContext().getString(R.string.notification_relation_liked_text2)+payload.get("eventThanksSize")+").")
                 .setContentInfo("Info")
@@ -178,34 +151,40 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     /**
      * Create and show notification about new coverage
-     *
-     * @param data Map which has the message data in it
+     * @param payload Map which has the message payload in it about created coverage
      */
-    private void showNewCoverageNotification(Map<String, String> data) {
+    private void showNewCoverageNotification(Map<String, String> payload) {
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_NEW_COVERAGE_CHANNEL_ID, "New coverage notification", NotificationManager.IMPORTANCE_DEFAULT);
+            // Configure the notification channel.
+            notificationChannel.setDescription("Notification channel for new coverage notification");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.GREEN);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
         Intent intent = new Intent(this, EventActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        // Create the pending intent to launch the activity
 
-
-        String place = data.get("place");
-        String windPower = data.get("wind_power");
-        String waveSize = data.get("wave_size");
-        String comment = data.get("comment");
-        Event newEvent = new Event(getApplicationContext(), data.get("id"), data.get("username"), data.get("uid"), place, Integer.valueOf(data.get("country")), Integer.valueOf(data.get("type")), Integer.valueOf(windPower), Double.valueOf(waveSize), Integer.valueOf(data.get("conditions")), comment, data.get("photoUrl"), "user_icon_goya_banzai_24");
-        newEvent.setmSharesCounter(Integer.valueOf(data.get("sharesCounter")));
-        //Check if it's a new user or not
-
+        //Data to show in notification
+        String place = payload.get("place");
+        String windPower = payload.get("wind_power");
+        String waveSize = payload.get("wave_size");
+        String comment = payload.get("comment");
+        //Make event to put to pendingIntent
+        Event newEvent = new Event(getApplicationContext(), payload.get("id"), payload.get("username"), payload.get("uid"), place, Integer.valueOf(payload.get("country")), Integer.valueOf(payload.get("type")), Integer.valueOf(windPower), Double.valueOf(waveSize), Integer.valueOf(payload.get("conditions")), comment, payload.get("photoUrl"), "user_icon_goya_banzai_24");
+        newEvent.setmSharesCounter(Integer.valueOf(payload.get("sharesCounter")));
+        //Check if user has uid in sharedPref
         if (checkUser()) {
-            //If we get user data from firebase lets make notification
+            //If we got user uid from sharedPref
             intent = putCoverageToIntent(intent, newEvent, mWindsurfer, getApplicationContext());
-            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-            final String uid = sharedPrefs.getString(getString(R.string.user_uid_shared_preference), "0");
-            mWindsurfer.setUid(uid); // Set only uid, other windsurfer data will be set in EventActivity
+            //Set temporary user username to download payload in eventActivity
             mWindsurfer.setUsername("NOTIFICATION");
             putWindsurferToIntent(intent, mWindsurfer, getApplicationContext());
 
-            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_NEW_COVERAGE_CHANNEL_ID);
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                     PendingIntent.FLAG_ONE_SHOT);
             notificationBuilder.setAutoCancel(true)
@@ -215,19 +194,20 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                     .setColor(getColor(R.color.notifications_color))
                     .setLargeIcon(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.notification_new_coverage))
                     .setTicker(getApplicationContext().getString(R.string.notification_new_coverage_title))
-                    //     .setPriority(Notification.PRIORITY_MAX)
                     .setContentTitle(getApplicationContext().getString(R.string.notification_new_coverage_title) + place)
                     .setContentText(place + " - " + windPower + "kn, " + waveSize + "m, " + comment)
-                    .setContentInfo("Info")
                     .setTimeoutAfter(28800000L) //After 8h notification should be canceled
                     // Set the intent that will fire when the user taps the notification
                     .setContentIntent(pendingIntent)
                     .setAutoCancel(true);
-            notificationManager.notify(data.hashCode(), notificationBuilder.build());
+            notificationManager.notify(payload.hashCode(), notificationBuilder.build());
         }
     }
 
-    //Function for gets user data using shared pref
+    /**
+     * Function for gets user uid from sharedPref
+     * @return true if user has uid in sharedPref
+     */
     private boolean checkUser() {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         final String uid = sharedPrefs.getString(getString(R.string.user_uid_shared_preference), "0");
@@ -235,6 +215,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             //Don't create notification
             return false;
         }
+        mWindsurfer.setUid(uid); // Set only uid, other windsurfer data will be set in EventActivity
         return true;
     }
 }
