1e64c03c2fd325cf129240b3b1c4e2b8ed5db440
Jan Gąsienica-Józkowy <jgjozkowy@gmail.com>
2019-05-25 20:56:56 +0200

    Event Archive Activity created.

---
+++ app/src/main/java/livewind/example/andro/liveWind/Archive/EventArchiveActivity.java
@@ -0,0 +1,12 @@
+package livewind.example.andro.liveWind.Archive;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EventArchiveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(livewind.example.andro.liveWind.R.layout.activity_catalog);
+    }
+}
